fe099671e0eaa5bad8a040beba7db06268d210d1
Mandy <chenjiafeng16@126.com>
2020-08-29 19:02:55 +0800

    [vue][mandy] feature : add Layout

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,68 @@
 <template>
+  <!-- <router-view /> -->
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <el-container>
+      <el-header>Rbac系统</el-header>
+      <el-container>
+        <el-main>
+          <el-row>
+            <el-col :span="4">
+              <indexNav></indexNav>
+            </el-col>
+            <el-col :span="20"><indexMain></indexMain></el-col>
+          </el-row>
+        </el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
+import indexNav from "./components/Layout/indexNav";
+import indexMain from "./components/Layout/indexMain";
 export default {
-  name: 'App'
-}
+  name: "App",
+  components: {
+    indexNav,
+    indexMain
+  }
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/* @import url("//unpkg.com/element-ui@2.13.2/lib/theme-chalk/index.css"); */
+/* @import url("element-ui/lib/theme-chalk/index.css"); */
+/* @import "element-ui/lib/theme-chalk/index.css"; */
+html,
+body,
+#app,
+.el-container {
+  padding: 0px;
+  margin: 0px;
+  height: 100%;
 }
+
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+  padding: 0px;
+}
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: left;
+  line-height: 60px;
+}
+
+
 </style>
---
+++ src/components/Layout/indexMain.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="index_main">2333</div>
+</template>
+
+<script>
+export default {
+  name: "indexMain"
+};
+</script>
+
+<style>
+.index_main {
+  height: 700px;
+  background-color: #6fa4d8;
+}
+</style>
